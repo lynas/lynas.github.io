82567d1e7f9c7ebf174fa33e02e25b953ea38d3f
LynAs <szlynas@gmail.com>
2015-12-15 18:43:00 +0600

    movie anime list added

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,9 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('DataController', ['$scope', '$http', function ($scope, $http) {
     $scope.movies = [
-        {name:'Avatar',year:2010,myRating:10/10},
-        {name:'Hitman',year:2010,myRating:10/10},
-        {name:'Avatar',year:2010,myRating:10/10},
-        {name:'Zoomanzi',year:2010,myRating:10/10}
+        {name:'Avatar',year:2010,myRating:10/10  , status: 'watched'},
+        {name:'Hitman',year:2010,myRating:10/10  , status: 'unwatched'},
+        {name:'Avatar',year:2010,myRating:10/10  , status: 'unwatched'},
+        {name:'Zoomanzi',year:2010,myRating:10/10, status: 'watched'}
     ];
 } ]);
