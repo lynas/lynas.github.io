344f688ef19ddb74cad3d2b985eeab4656d44090
LynAs <szlynas@gmail.com>
2015-12-16 11:18:35 +0600

    month added

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,9 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('DataController', ['$scope', '$http', function ($scope, $http) {
     $scope.movies = [
-        {name:'Avatar',year:2010,myRating:10/10  , status: 'watched'},
-        {name:'Hitman',year:2010,myRating:10/10  , status: 'unwatched'},
-        {name:'Avatar',year:2010,myRating:10/10  , status: 'unwatched'},
-        {name:'Zoomanzi',year:2010,myRating:10/10, status: 'watched'}
+        {name:'Fantastic Beasts',       year:2010,      month:'November',    myRating:10/10,       status: 'watched'},
+        {name:'Hitman',                 year:2010,      month:'November',    myRating:10/10,       status: 'unwatched'},
+        {name:'Avatar',                 year:2010,      month:'November',    myRating:10/10,       status: 'unwatched'},
+        {name:'Zoomanzi',               year:2010,      month:'November',    myRating:10/10,       status: 'watched'}
     ];
 } ]);
