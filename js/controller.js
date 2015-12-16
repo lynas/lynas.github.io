d1b877a1ffe22b2f67b660c32fdb34a32d79d148
LynAs <szlynas@gmail.com>
2015-12-16 11:22:33 +0600

    movie updated

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,9 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('DataController', ['$scope', '$http', function ($scope, $http) {
     $scope.movies = [
-        {name:'Fantastic Beasts',       year:2010,      month:'November',    myRating:10/10,       status: 'watched'},
-        {name:'Hitman',                 year:2010,      month:'November',    myRating:10/10,       status: 'unwatched'},
-        {name:'Avatar',                 year:2010,      month:'November',    myRating:10/10,       status: 'unwatched'},
-        {name:'Zoomanzi',               year:2010,      month:'November',    myRating:10/10,       status: 'watched'}
+        {name:'Fantastic Beasts',       year:2016,      month:'November',    myRating:10/10,       status: 'unwatched'},
+        {name:'How To Be Single',       year:2016,      month:'February',    myRating:10/10,       status: 'unwatched'},
+        {name:'Central Intelligence',   year:2016,      month:'June',        myRating:10/10,       status: 'unwatched'},
+        {name:'Batman v Superman: Dawn of Justice',               year:2016,      month:'March',    myRating:10/10,       status: 'unwatched'}
     ];
 } ]);
