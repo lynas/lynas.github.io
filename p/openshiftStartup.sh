9df30684bc0507543f0b48486b848d7936133768
LynAs Sazzad <szlynas@gmail.com>
2016-02-24 22:23:22 +0600

    openshift start script added

---
+++ p/openshiftStartup.sh
@@ -0,0 +1,22 @@
+#wget http://lynas.github.io/p/openshiftStartup.sh
+
+cd $OPENSHIFT_DATA_DIR
+export JAVA_HOME=/etc/alternatives/java_sdk_1.8.0 
+export PATH=$JAVA_HOME/bin:$PATH
+
+
+wget http://www.eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
+
+unzip apache-maven-3.3.9-bin.zip 
+
+export M2_HOME=$OPENSHIFT_DATA_DIR/apache-maven-3.3.9
+export PATH=$M2_HOME/bin:$PATH
+
+mkdir -p $OPENSHIFT_DATA_DIR/.m2
+export MAVEN_OPTS="-Dmaven.repo.local=$OPENSHIFT_DATA_DIR/.m2"
+
+
+
+
+#java -jar target/*.jar --server.port=${OPENSHIFT_DIY_PORT} --server.address=${OPENSHIFT_DIY_IP}
+
